5edb02ec13bb17887d0f52197fc15895d8fd0b60
TarunBlaze <40628308+blaze021@users.noreply.github.com>
2022-09-29 11:39:34 +0000

    Update get_blob_file.py

--- get_blob_file.py
+++ get_blob_file.py
@@ -6,15 +6,20 @@ STORAGEACCOUNTKEY = subprocess.getoutput("terraform output storage_primary_acces
 CONTAINERNAME = subprocess.getoutput("terraform output storage_container_name")[1:-1] #"<CONTAINER_NAME>"
 BLOBNAME = subprocess.getoutput("terraform output blob_name")[1:-1] #"<BLOB_FILE_NAME>"
 
-blob_service_client_instance = BlobServiceClient(
-    account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
 
-blob_client_instance = blob_service_client_instance.get_blob_client(
-    CONTAINERNAME, BLOBNAME, snapshot=None)
-
-blob_data = blob_client_instance.download_blob()
-
-with open("index.html", "wb") as f:
-   blob_data.readinto(f)
+try:
+    
+    blob_service_client_instance = BlobServiceClient(
+        account_url=STORAGEACCOUNTURL, credential=STORAGEACCOUNTKEY)
+    
+    blob_client_instance = blob_service_client_instance.get_blob_client(
+        CONTAINERNAME, BLOBNAME, snapshot=None)
+    
+    blob_data = blob_client_instance.download_blob()
+    
+    with open("index.html", "wb") as f:
+        blob_data.readinto(f)
+except Exception as e:
+    print(e)
 # data = blob_data.readall()
 # print(data)
